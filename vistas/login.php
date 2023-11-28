0b2d585ac41b67ffb7aadfbcc51fad85d12feba0
piki1489 <piki1489@hotmail.com>
2023-11-28 18:33:53 +0100

    terminado

--- vistas/login.php
+++ vistas/login.php
@@ -6,20 +6,32 @@
         <div class="field">
             <label class="label">Usuario</label>
             <div class="control">
-                <input class="input" type="text" name="login_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required>
+                <input class="input" type="text" name="login_usuario" pattern="[a-zA-Z0-9\$@.-]{7,100}" maxlength="100" required>
+
+
             </div>
         </div>
 
         <div class="field">
             <label class="label">Clave</label>
             <div class="control">
-                <input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required>
+                <input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9\$@.-]{7,100}" maxlength="100" required>
+
+
             </div>
         </div>
 
         <p class="has-text-centered mb-4 mt-3">
             <button type="submit" class="button is-info is-rounded">Iniciar sesion</button>
         </p>
+
+        <?php
+        if (isset($_POST['login_usuario']) && isset($_POST['login_clave'])) {
+            require_once "./php/main.php";
+            require_once "./php/iniciar_sesion.php";
+        }
+        ?>
     </form>
+
 
 </div>
